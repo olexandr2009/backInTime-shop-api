69c1d76c10884685ed050cafcbc187df5d4fc138
olexandr2009 <hristevich.ua@gmail.com>
2024-02-15 21:39:12 +0200

    optimized imports

--- src/main/java/ua/shop/backintime/user/controller/UserController.java
+++ src/main/java/ua/shop/backintime/user/controller/UserController.java
@@ -13,7 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
