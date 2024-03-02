ae8147777438a8c5ed73678fce764d338bf5ebf2
olexandr2009 <hristevich.ua@gmail.com>
2024-03-02 19:08:23 +0200

    test find

--- src/main/resources/db/migration/V1__init_db.sql
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -1,19 +1,20 @@
 CREATE TABLE IF NOT EXISTS users(
-    id BIGSERIAL PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(100) NOT NULL CHECK(LENGTH(password)>=8),
-    active_token VARCHAR UNIQUE,
-    last_login_date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    last_updated_date DATE NOT NULL,
-    created_date DATE NOT NULL
+id BIGSERIAL PRIMARY KEY,
+first_name VARCHAR(50) NOT NULL,
+last_name VARCHAR(50) NOT NULL,
+email VARCHAR(50) UNIQUE NOT NULL,
+password VARCHAR(100) NOT NULL CHECK(LENGTH(password)>=8),
+active_token VARCHAR UNIQUE,
+last_login_date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+last_updated_date DATE NOT NULL,
+created_date DATE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS roles(
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
+id BIGSERIAL PRIMARY KEY,
+name VARCHAR(50) NOT NULL
 );
+
 
 CREATE TABLE IF NOT EXISTS user_roles (
     id BIGSERIAL PRIMARY KEY,
@@ -23,4 +24,4 @@ CREATE TABLE IF NOT EXISTS user_roles (
     CONSTRAINT role_user_fk FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE
 );
 
-INSERT INTO roles (name) VALUES ('ROLE_ADMIN'), ('ROLE_TESTER'), ('ROLE_USER');
+INSERT INTO roles (name) VALUES ('ADMIN'),('AUTH_USER'), ('USER');
